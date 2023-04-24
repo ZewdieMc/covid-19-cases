ca6c895bf9b831a57420a47665f333220da2268b
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-24 12:26:00 +0000

    Rename initalState to  initialState

--- src/redux/covid/covidSlice.js
+++ src/redux/covid/covidSlice.js
@@ -1,42 +1,54 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import client from '../client/client';
+import client from '../../client/client';
 
 const fetchCases = createAsyncThunk(
   'covid/fetchCases',
   async (_, thunkAPI) => {
     try {
       const response = client.get('countries');
-      return response.data;//
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: 'Cannot fetch covid data' });
     }
   },
 );
 
-const initalState  = {
-    cases: [],
-    isLoading: false,
-    error:'',
-}
+const initialState = {
+  cases: [],
+  isLoading: false,
+  error: '',
+};
 
 const covidSlice = createSlice({
-    name: 'covid',
-    initalState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(
-            fetchCases.fulfilled, (state, action) => {
-                const caseList = action.payload.slice(0,12).map(
-                    (case) => ({
-                        country: case.country,
-                        countryInfo: case.countryInfo,
-                        cases: case.cases,
-                        deaths: case.deaths,
-                        recovered: case.recovered,
-                        active: case.active
-                    })
-                );
-            }
-        )
-    }
+  name: 'cases',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCases.fulfilled, (state, action) => {
+      const caseList = action.payload.map((covidCase) => ({
+        country: covidCase.country,
+        countryInfo: covidCase.countryInfo,
+        cases: covidCase.cases,
+        deaths: covidCase.deaths,
+        recovered: covidCase.recovered,
+      }));
+      return ({
+        ...state,
+        isLoading: false,
+        rockets: caseList,
+      });
+    })
+      .addCase(fetchCases.pending, (state) => ({
+        ...state,
+        isLoading: true,
+        error: '',
+      }))
+      .addCase(fetchCases.rejected, (state, action) => ({
+        ...state,
+        error: action.payload.error,
+        isLoading: false,
+      }));
+  },
 });
+
+export default covidSlice.reducer;
