708a993aa14d981ec3e80fe280034946eae4bfdf
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-25 03:15:09 +0000

    Add NotFound page into the route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import CovidLayOut from './components/CovidLayOut';
 import Detail from './components/Detail';
+import PageNotFound from './components/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path=":name" element={<Detail />} />
         </Route>
       </Route>
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
