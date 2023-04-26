7d3247cc47e69a2fee964b2e4f5ef00ee0b80082
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-26 08:54:39 +0000

    Style the list of cases

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,25 @@
 import { useOutletContext } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Case from './Case';
 
 const Home = () => {
   const { cases } = useOutletContext();
+  const { isLoading } = useSelector((state) => state.cases);
+  const { error } = useSelector((state) => state.cases);
+
   return (
-    <div>
-      <h1>
-        Home
-        {' '}
-        { cases.length }
-      </h1>
+    <div className="casesList">
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {cases && (
+        <div className="cases">
+          {
+            cases.map((singleCase) => (
+              <Case key={singleCase.id} covidCase={singleCase} />
+            ))
+          }
+        </div>
+      )}
     </div>
   );
 };
